d2551610d7c9d014715fe5589173258a854fb4ad
Jim Chavez <jimstanley.chavez@umontana.edu>
2018-12-05 00:38:47 -0700

    stuff
    stuff

---
+++ Bag of Holding (Final)/Assets/Scripts/ChangeGear.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class ChangeGear : MonoBehaviour
+{
+    private Equipment equipmentScript; 
+
+    private void Start()
+    {
+        equipmentScript = GetComponent<Equipment>();
+        //creates initial equipment list
+        equipmentScript.InitializeEquipptedItemsList();
+        //initally equips pants so the model isn't naked
+       EquipItem("Legs", "pants"); 
+    }
+
+    public void EquipItem(string itemType, string itemSlug)
+    {
+        //iterates through the equipment list 
+        for (int i = 0; i < equipmentScript.equippedItems.Count; i++)
+        {
+            //finds itme at specified index
+            if (equipmentScript.equippedItems[i].ItemType == itemType)
+            {
+                //changes index to the index of the item slug
+                equipmentScript.equippedItems[i] = ItemDatabase.instance.FetchItemBySlug(itemSlug);
+                //updates equipment list
+                equipmentScript.AddEquipment(equipmentScript.equippedItems[i]);
+                break;
+            }
+        }
+    }
+
+    public void UnequipItem(string itemType, string itemSlug)
+    {
+        //iterates through the equipment list
+        for (int i = 0; i < equipmentScript.equippedItems.Count; i++)
+        {
+            //finds item at specified index
+            if (equipmentScript.equippedItems[i].ItemType == itemType)
+            {
+                //updates the equipment list
+                equipmentScript.RemoveEquipment(equipmentScript.equippedItems[i]);
+                break;
+            }
+        }
+    }
+}
---
+++ Bag of Holding (Final)/Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+public void StartScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+public void MainApplication()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
